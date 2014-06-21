7d0d49ca04a11a1ee7f05e2c4647ae8a6fe1198f
RJ <ctrlaltreboot@gmail.com>
2014-06-21 21:01:53 +0800

    Add Berksfile

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,7 @@ license          'GPLv2'
 description      'Installs/Configures rnginx'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+
+depends   'apt'
+depends   'ark'
