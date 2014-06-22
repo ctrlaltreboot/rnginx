d116096f51a8d70fd70b8ccf9a6591c8cb456146
RJ <ctrlaltreboot@gmail.com>
2014-06-22 08:27:02 +0800

    Trim spec for common packages

--- spec/common_packages_spec.rb
+++ spec/common_packages_spec.rb
@@ -1,20 +1,14 @@
 require 'chefspec'
 
-describe 'rnginx::common_packages' do
+describe 'rnginx::binary' do
   let (:chef_run) {
     ChefSpec::ChefRunner.new(
       platform:'ubuntu', version:'12.04'
       ).converge(described_recipe)
   }
 
-  it 'installs libpcre3' do
-    expect(chef_run).to install_package 'libpcre3'
-  end
-  it 'installs libpcre3-dev' do
-    expect(chef_run).to install_package 'libpcre3-dev'
-  end
-  it 'installs libssl-dev' do
-    expect(chef_run).to install_package 'libssl-dev'
+  it 'installs nginx' do
+    expect(chef_run).to install_package 'nginx'
   end
 
 end
