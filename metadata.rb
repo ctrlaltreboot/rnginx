b581682fa2e2a259825e18366869138f58ac574f
RJ <ctrlaltreboot@gmail.com>
2014-06-22 08:38:07 +0800

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ctrlaltreboot@gmail.com'
 license          'GPLv2'
 description      'Installs/Configures rnginx'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.2'
 
 
 depends   'apt'
